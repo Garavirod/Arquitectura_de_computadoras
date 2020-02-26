ad205f8062e37dd550b27932bbb4de735551042d
Garavirod <rodrigogarciaavila26@gmail.com>
2020-02-25 22:45:23 -0600

    mult8bit00

---
+++ PRIMER PARCIAL/key00/contring00.vhdl
@@ -0,0 +1,11 @@
+
+
+
+entity contring00 is
+	port(
+		clkcr: in std_logic;
+		ecr: in std_logic
+		outcr: out std_logic_vector(3 downto 0)
+	);
+	
+end contring00;
--- PRIMER PARCIAL/word00/word00.vhdl
+++ PRIMER PARCIAL/word00/word00.vhdl
@@ -2,14 +2,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 library lattice;
 use lattice.all;
-
+use packageword00.all;
 
 entity word00 is
 	port(
 		clk0: inout std_logic;
 		cdiv0: in std_logic_vector(4 downto 0);
-		encr: in std_logic;
-		outtransist0: out std_logic_vector(3 downto 0);
+		en0: in std_logic;
+		outtransist0: inout std_logic_vector(3 downto 0);
 		outdisplay0: out std_logic_vector(6 downto 0)
 	);
 	
@@ -18,7 +18,17 @@ end word00;
 
 architecture word0 of word00 is
 begin
-	--W00: osc00 port map();
-	--W01: contring00 port map();
-	--W02: coder00 port map();
+	W00: osc00 port map(
+		oscout0 => clk0,
+		indiv0 => cdiv0
+	);
+	W01: contring00 port map(
+		clkcr =>clk0,
+		encr => en0,
+		outcr => outtransist0
+	);
+	W02: coder00 port map(
+		incontcd => outtransist0,
+		outcd => outdisplay0
+	);
 end word0;
